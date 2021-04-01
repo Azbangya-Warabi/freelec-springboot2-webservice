0c174d5181a69060a5ed2d3d35467ac9aeadacf2
wonsik <whdnjstlr321@naver.com>
2021-04-01 17:36:47 +0900

    create board function

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,12 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 // 데이터베이스를 접근하게 해주는 역할, JpaRepository<Ebtity 클래스, PK타입>을 상속하면 기본적인 CRUD메서드를 자동으로 생성
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
